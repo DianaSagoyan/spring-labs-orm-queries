93d395358a84f254e46a76c0c6a77ebec1338a6a
DianaSagoyan <dianasagoyan@yahoo.com>
2022-11-03 20:01:17 -0500

    Completed CartItemRepository query tasks

--- lab-07-orm-queries/src/main/java/com/cydeo/lab07ormqueries/repository/CartItemRepository.java
+++ lab-07-orm-queries/src/main/java/com/cydeo/lab07ormqueries/repository/CartItemRepository.java
@@ -2,17 +2,36 @@ package com.cydeo.lab07ormqueries.repository;
 
 
 import com.cydeo.lab07ormqueries.entity.CartItem;
+import com.cydeo.lab07ormqueries.entity.Discount;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.math.BigInteger;
+import java.util.List;
 
 @Repository
 public interface CartItemRepository extends JpaRepository<CartItem, Long> {
 
     //Write a derived query to get count cart items
+    int countCartItemByCart();
+
     //Write a derived query to get cart items for specific cart state
+    List<CartItem> findByCart_CartState(String cartState);
+
     //Write a native query to get cart items for specific cart state and product name
+    @Query(nativeQuery = true, value = "select * from cart_item ci join cart c on ci.cart_id = c.id" +
+            "join product p on ci.product_id = p.id where c.cart_state = ?1 and p.name = ?2")
+    List<CartItem> retrieveCartItemWithSpecificStateAndProductName(String state, String productName);
+
     //Write a native query to get cart items for specific cart state and without discount
+    @Query(nativeQuery = true, value = "select * from cart_item ci join cart c on ci.cart_id = c.id" +
+            "where c.cart_state = ?1 and c.discount_id = null")
+    List<CartItem> retrieveCartItemsWithCartSateAndNoDiscount(String state);
+
     //Write a native query to get cart items for specific cart state and with specific Discount type
+    @Query(nativeQuery = true, value = "select * from cart_item ci join cart c on ci.cart_id = c.id" +
+            "where c.cart_state = ?1 and c.dicount_id <> ?2")
+    List<CartItem> retrieveCartItemsWithCartSateAndNoSpecificDiscount(String state, Discount discount);
+
 }
