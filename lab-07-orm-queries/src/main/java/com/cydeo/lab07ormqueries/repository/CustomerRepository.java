5c3ea7fe72c4a3ca8aeb1e4fb64794f2fdcdf0f0
DianaSagoyan <dianasagoyan@yahoo.com>
2022-11-05 22:07:58 -0500

    Completed CustomerRepository query tasks

--- lab-07-orm-queries/src/main/java/com/cydeo/lab07ormqueries/repository/CustomerRepository.java
+++ lab-07-orm-queries/src/main/java/com/cydeo/lab07ormqueries/repository/CustomerRepository.java
@@ -2,13 +2,19 @@ package com.cydeo.lab07ormqueries.repository;
 
 import com.cydeo.lab07ormqueries.entity.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.math.BigInteger;
+import java.util.List;
 
 @Repository
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
     //Write a derived query to get all customer by id
+    List<Customer> findById(int id);
+
     //Write a JPQL query to get customer by email
+    @Query("select c from Customer c where c.email = ?1")
+    Customer retrieveByEmail(String email);
 
 }
