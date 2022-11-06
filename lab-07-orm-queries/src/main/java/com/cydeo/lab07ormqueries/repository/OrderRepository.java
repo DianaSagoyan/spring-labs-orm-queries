936ca447a59bf9a150d2ee15f27ad380ef3dc7de
DianaSagoyan <dianasagoyan@yahoo.com>
2022-11-05 22:27:25 -0500

    Working on OrderRepository query tasks

--- lab-07-orm-queries/src/main/java/com/cydeo/lab07ormqueries/repository/OrderRepository.java
+++ lab-07-orm-queries/src/main/java/com/cydeo/lab07ormqueries/repository/OrderRepository.java
@@ -2,22 +2,40 @@ package com.cydeo.lab07ormqueries.repository;
 
 import com.cydeo.lab07ormqueries.entity.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.math.BigInteger;
+import java.util.List;
 
 
 @Repository
 public interface OrderRepository extends JpaRepository<Order, Long> {
     //Write a derived query to get top 5 orders by order by total price desc
+    List<Order> findTop5ByTotalPriceOrderByDesc();
+
     //Write a derived query to get all orders by customer email
+    List<Order>findByCustomer_Email(String email);
+
     //Write a derived query to get all orders by specific payment method
-    //Write a derived query to get all orders by specific payment method
-    //Write a derived query to get all orders by specific customer email
+    List<Order> findByPaymentPaymentMethod(String method);
+
+
     //Write a derived query to check is there any orders by customer email
+    boolean existsByCustomer_Email(String email);
+
     //Write a native query to get all orders by specific product name
+    @Query(nativeQuery = true, value = "select * from orders o join product p") //???
+    List<Order> retrieveOrdersByProductName(String name);
+
     //Write a native query to get all orders by specific categoryId
+    
+
     //Write a derived query to get all orders by totalPrice and paidPrice are equals
+
+
     //Write a derived query to get all orders by totalPrice and paidPrice are not equals and discount is not null
+
+
 
 }
