49dcdea7efeb02fe3187df4c8aa508bba739186d
DianaSagoyan <dianasagoyan@yahoo.com>
2022-11-05 22:05:02 -0500

    Completed CategoryRepository query tasks

--- lab-07-orm-queries/src/main/java/com/cydeo/lab07ormqueries/repository/CategoryRepository.java
+++ lab-07-orm-queries/src/main/java/com/cydeo/lab07ormqueries/repository/CategoryRepository.java
@@ -5,9 +5,14 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.math.BigInteger;
+import java.util.List;
 
 @Repository
 public interface CategoryRepository extends JpaRepository<Category, Long> {
     //Write a derived query to get category by name
+    List<Category> findByName(String name);
+
     //Write a derived query to get top 3 categories order by name desc
+    List<Category> findTop3ByNameOrderByDesc();
+
 }
